7954a9135ae69780806095effbc7332325e16475
Hyperchess Botauthor <hyperchessbotauthor@gmail.com>
2020-11-16 17:36:37 +0000

    Dockerfile.sh

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM cimg/base
+FROM ubuntu:18.04
 
 COPY . .
 
--- Dockerfile.sh
+++ Dockerfile.sh
@@ -1,4 +1,17 @@
+echo "update"
+sudo apt-get update -y > log
+
+echo "install curl"
+apt-get install curl -y > log
+
+echo "install sudo"
+apt-get install sudo -y > log
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+echo "install git"
+sudo apt-get install git -y > log
+
 echo "load tablebases"
-git clone https://github.com/hyperbotauthor/syzygy /syzygy
+sudo git clone https://github.com/hyperbotauthor/syzygy /syzygy
 rm -rf /syzygy/.git
 ls /syzygy
